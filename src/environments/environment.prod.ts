fc5de1fe6792f834e70e57d785bb1f4c7213fabd
Karen <karenbj15@hotmail.com>
2025-06-12 17:36:31 -0500

    add_environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+  apiKey: "AIzaSyBuxl_ZqPplDryDkal7M10VHKmkN1rcxkc",
+  authDomain: "todolistapp-ecb80.firebaseapp.com",
+  projectId: "todolistapp-ecb80",
+  storageBucket: "todolistapp-ecb80.firebasestorage.app",
+  messagingSenderId: "872443929695",
+  appId: "1:872443929695:web:438488ba133d02547067ea"
+}
+
 };
